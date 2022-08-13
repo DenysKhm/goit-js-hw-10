0f153b8b2dd711c18c57e47e3b2ec8ce400c1329
DenysKhm <khmelevskyy7@gmail.com>
2022-08-13 20:41:49 +0300

    hw10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,5 +2,15 @@ const MAIN_URL = `https://restcountries.com/v3.1/name`;
 const FILTER = `fields=name,capital,population,flags,languages`;
 
 export default function fetchCountries(name) {
-  fetch('https://restcountries.com/v3.1/name/{name}');
+  if (name) {
+    return fetch(`${MAIN_URL}/${name}?${FILTER}`).then(onSuccessFetch);
+  }
+}
+
+function onSuccessFetch(response) {
+  console.log(response.status);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return response.json();
 }
